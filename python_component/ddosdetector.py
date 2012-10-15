aaa490b9adeee2a3979f50651d988837e76f4671
Rodrigo de Souza Braga <rodrigogrow@gmail.com>
2012-10-15 14:07:55 -0700

    add GNU GPL License

--- python_component/ddosdetector.py
+++ python_component/ddosdetector.py
@@ -1,20 +1,22 @@
-# Developed by Rodrigo Braga
-# Copyright 2010 (C) LabCIA, UFAM.
-# 
-# This file is part of NOX.
-# 
-# NOX is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# NOX is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with NOX.  If not, see <http://www.gnu.org/licenses/>.
+#  File ddosdetector.py, 
+#  brief: Nox component to get OF flows and seek 
+#         for ddos traffic patterns.
+#
+#  Copyright (C) 2010  Rodrigo de Souza Braga
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import os.path
 from nox.lib.core import *
